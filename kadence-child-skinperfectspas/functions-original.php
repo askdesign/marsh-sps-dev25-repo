af3344cc223107ee56474aa651c1b7929efc2b83
Anne S. Katzeff <anne@askdesign.biz>
2025-05-02 15:47:14 -0400

    Dashicons
    • Enqueue dashicons to display on front-end

--- kadence-child-skinperfectspas/functions-original.php
+++ kadence-child-skinperfectspas/functions-original.php
@@ -12,11 +12,3 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
 /**
  * Add custom functions here
  */
-
-//To enable dashicons on your front-end, you’ll need to enqueue the dashicons stylesheet. 
-//You can do this by adding a small snippet of code to your child theme’s functions.php file:
- 
-function load_dashicons_frontend() {
-    wp_enqueue_style('dashicons');
-}
-add_action('wp_enqueue_scripts', 'load_dashicons_frontend');
